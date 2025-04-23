fccdc66bbf9e30397d9ec0577a82cb725330521d
malmarzo <m.h.almarzooq@gmail.com>
2025-04-23 10:00:42 +0300

    Suppressed hydration warning

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -23,7 +23,7 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
